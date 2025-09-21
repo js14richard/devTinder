86bc0862468d88a725d2080f0c05e72654c57803
Siril Richard <js14richard@gmail.com>
2025-09-21 15:54:28 +0530

    basic route setup

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.use("/users", (req, res)=> {
-    res.send('Users route');
+app.get("/users", (req, res)=> {
+    res.send({
+        name: 'Siril',
+        age: 24,
+    });
 });
 
-app.use("/admin", (req, res)=>{
-    res.send('Admin route');
+app.get("/user/:id", (req, res) => {
+    const userId = req.params.id;
+    res.send(`User ID: ${userId}`);
+});
+
+app.post("/users", (req, res) =>{
+    res.send('POST Users route');
+});
+
+// This will match all HTTP methods (GET, POST, PUT, DELETE, etc.) for the /users path
+// Won't be used if specific method routes are defined above
+app.use("/users", (req, res)=> {
+    res.send('Users route');
 });
 
 // Add this route at the end to avoid overriding other routes as / matches all paths
