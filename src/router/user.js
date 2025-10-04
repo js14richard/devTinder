b07526c385b3faec8f4e3066149067f964b52b8e
Siril Richard <js14richard@gmail.com>
2025-10-04 23:58:29 +0530

    fetch user connections

--- src/router/user.js
+++ src/router/user.js
@@ -4,15 +4,16 @@ const {userAuth} = require("../middlewares/auth");
 
 const {ConnectionRequest} = require("../models/connectionRequest");
 
+const ALLOWED_CONNECTION_FIELDS_TO_DISPLAY = ["firstName", "lastName", "skills", "gender", "about", "age", "photoUrl"];
+
 userRouter.get("/user/requests/received", userAuth, async (req, res)=> {
 
     try{
-        const ALLOWED_CONNECTION_SENDER_FIELDS_TO_DISPLAY = ["firstName", "lastName", "skills", "gender", "about", "age", "photoUrl"];
 
         const pendingRequests = await ConnectionRequest.find({
             connectionReceiverId: req.currentUser._id,
             connectionRequestStatus: "interested" // retrieve the data only when users has sent me a connection request(interest)
-        }).populate("connectionSenderId", ALLOWED_CONNECTION_SENDER_FIELDS_TO_DISPLAY);
+        }).populate("connectionSenderId", ALLOWED_CONNECTION_FIELDS_TO_DISPLAY);
 
         console.log(pendingRequests);
 
@@ -33,6 +34,59 @@ userRouter.get("/user/requests/received", userAuth, async (req, res)=> {
     }
 });
 
+
+
+
+// List my connections
+
+userRouter.get("/user/connections", userAuth, async (req, res)=>{
+
+    try{
+        const loggedInUserId = req.currentUser._id
+        const userConnections = await ConnectionRequest.find({
+            // Retrieves all the accepted connections either it can be sent by user or sent to the user
+            $or:[
+                {connectionSenderId : loggedInUserId, connectionRequestStatus: "accepted"}, 
+                {connectionReceiverId : loggedInUserId, connectionRequestStatus: "accepted"}
+            ]
+        }).populate("connectionSenderId", ALLOWED_CONNECTION_FIELDS_TO_DISPLAY)
+        .populate("connectionReceiverId", ALLOWED_CONNECTION_FIELDS_TO_DISPLAY);
+
+        // now userConnections will ALLOWED_CONNECTION_FIELDS_TO_DISPLAY the fields for both sender and receiver
+        // but we only need the other person's details
+        // So if the user sent the request to someone we need to display Receiver details 
+        // If someone else sent the request to user we need to display Sender details
+        
+        const connectionDataToDisplay = userConnections.map((row)=>{
+            if(loggedInUserId.equals(userConnections.connectionSenderId._id)){
+                return row.connectionReceiverId
+            } else{
+                return row.connectionSenderId
+            }
+        })
+
+        if(!userConnections.length){
+            return res.status(404)
+                .json({
+                    message: "User has no connections"
+                })
+        }
+
+        return res.json({
+            message: "Fetched user connections sucessfully",
+            data: connectionDataToDisplay
+        })
+
+
+
+    } catch(err){
+        console.error(err.stack);
+        return res.status(500)
+            .json({
+                message:"Error while fetching user connections"
+            })
+    }
+})
 
 
 module.exports = userRouter;
